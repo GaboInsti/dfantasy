0475b24d5df6ecb56f48498c3dbfc860c13fd46a
Gabo <17170898@itculiacan.edu.mx>
2022-11-30 21:41:57 -0600

    Bootstrap agregado y Creación de componente Login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dfantasy';
+  title: string = 'Diversiones sfantasy';
 }
+
